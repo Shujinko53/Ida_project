5cc3db25e388495d4155718c4aac37ff1e56139c
Shujinko53 <shujinko53@yandex.ru>
2021-08-16 11:26:18 +0300

    fix errors

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,20 @@ const itemsDelete = document.querySelectorAll('.list-item__delete'),
       item = document.querySelectorAll('.list-item'),
       items = document.querySelectorAll('.list-item');
 
-item.addEventListener('mouseover', ()=> {
-itemDelete.style.display = "block";
-})
-
-itemDelete.addEventListener('click', ()=> {
-    itemDelete.style.display = "none";
+items.forEach((item, i) => {
+    item.addEventListener('mouseover', () => {
+        itemsDelete[i].style.display = "block";
+    });
+    item.addEventListener('mouseout', () => {
+        itemsDelete[i].style.display = "none";
+    });
 });
+      
+itemsDelete.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        items[i].style.display = "none";
+    })
+})
 
 // валидация формы
 
@@ -18,7 +25,6 @@ const form = document.querySelector('.form'),
       formItems = document.querySelectorAll('.form__item_required'),
       formInputs = document.querySelectorAll('.form__item_required input'),
       errorMsgs = document.querySelectorAll('.errorMsg'),
-    //   requiredInputs = document.querySelectorAll('.form__item_required input'),
       formBtn = document.querySelector('.button_form');
 
 
@@ -33,15 +39,19 @@ form.addEventListener('submit', (evt) => {
             errorMsgs[i].style.display = 'block';
 
             setTimeout(function() {
-                formBtn.click();
                 form.setAttribute('novalidate', true);
+
               }, 0);
         } else {
             formItems[i].classList.remove('form__item_required-active');
             errorMsgs[i].style.display = 'none';
-            form.submit();
             formBtn.style.background = '#7BAE73';
             formBtn.style.color = '#ffffff';
+
+            formBtn.addEventListener('click', () => {
+                form.submit();
+            });
+            
         }
         
     })
